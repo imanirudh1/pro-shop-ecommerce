60da9183f7f781e8503b305322f40092ff9e5361
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-10-08 19:48:02 +0530

    Added header using bootstarp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-
+import React from 'react'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import {Container} from 'react-bootstrap'
 function App() {
   return (
     <>
-      <h1>Welcome to Proshop</h1>
+      <Header />
+      <main>
+        <Container className='py-3'>
+
+        <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
