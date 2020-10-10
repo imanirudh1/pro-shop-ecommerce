35306612acea5c47453c4517735070c1b972321d
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-10-10 23:42:35 +0530

    Added link to products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from './component/Footer'
 import Header from './component/Header'
 import {Container} from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 function App() {
   return (
     <Router>
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Container className='py-3'>
           <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' exact component={ProductScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import {Container,Navbar,Nav} from 'react-bootstrap'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 const Header = () => {
   return <header>
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+        <LinkContainer to='/'>
+        <Navbar.Brand >ProShop</Navbar.Brand>
+        </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <Nav.Link href="/cart">Cart</Nav.Link>
-      <Nav.Link href="/login">Sign In</Nav.Link>
+          <Nav className="ml-auto">
+          <LinkContainer to='/cart'>
+              <Nav.Link >Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/login'>
+              <Nav.Link>Sign In</Nav.Link>
+              </LinkContainer>
       
     </Nav>
     
