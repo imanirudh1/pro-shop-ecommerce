0c8c4dd98d851288afe984483f72074669693038
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-10-15 20:25:53 +0530

    added errorMiddleware to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,26 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
-
+import {errorHandeler,notFound} from './middleware/errorMiddleware.js'
 dotenv.config()
 connectDB()
 const app = express()
-
+// app.use((req, res, next) => {
+//   console.log(req.originalUrl)
+//   next()
+// })
 app.get('/', (req, res) => {
   res.send('API is running..')
 })
 
 app.use('/api/products',productRoutes)
+
+
+app.use(notFound)
+
+app.use(errorHandeler)
+
+
 
 const PORT = process.env.PORT || 5000
 
