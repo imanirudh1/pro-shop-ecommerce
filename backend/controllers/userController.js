ecc94579ededd34902c292ac4818e39b1138fe4f
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-04 22:38:14 +0530

    User Auth fixes some issues

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,15 @@ import User from '../model/userModel.js'
 import asyncHandeler from 'express-async-handler'
 
 //@desc Auth user and get a token
-//@route GET /api/users/login
+//@route POST /api/users/login
 //@access Public
-const authUser = asyncHandeler(async(req, res) => {})
+const authUser = asyncHandeler(async (req, res) => {
+  const { email, password } = req.body
+
+  res.send({
+    email,
+    password,
+  })
+})
+
+export { authUser }
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import {errorHandeler,notFound} from './middleware/errorMiddleware.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
-import {errorHandeler,notFound} from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
+
 dotenv.config()
 connectDB()
 const app = express()
-// app.use((req, res, next) => {
-//   console.log(req.originalUrl)
-//   next()
-// })
+
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
   res.send('API is running..')
 })
 
+
 app.use('/api/products',productRoutes)
+app.use('/api/users',userRoutes)
 
 
 app.use(notFound)
