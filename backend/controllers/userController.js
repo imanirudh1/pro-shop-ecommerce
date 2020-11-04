491efe8b6619a944ecbbc534254a0ae042fe6e58
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-04 21:46:53 +0530

    User Authentication End Point

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+import User from '../model/userModel.js'
+import asyncHandeler from 'express-async-handler'
+
+//@desc Auth user and get a token
+//@route GET /api/users/login
+//@access Public
+const authUser = asyncHandeler(async(req, res) => {})
