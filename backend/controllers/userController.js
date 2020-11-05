a6528e5642330062b6ce92eb34b3063249b7ce0e
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-06 00:38:09 +0530

    Refactored Code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,14 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
-
-//@desc Auth user and get a token
-//@route POST /api/users/login
-//@access Public
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
-  res.send(email,password)
+
+  res.send({ email, password })
   //   const user = await User.findOne({ email })
-    
+
   // if (user && (await user.matchPassword(password))) {
   //   res.json({
   //     _id: user._id,
--- backend/seeder.js
+++ backend/seeder.js
@@ -3,52 +3,55 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import users from './data/users.js'
 import products from './data/products.js'
-import User from './model/userModel.js'
-import Order from './model/orderModel.js'
-import Product from './model/productModel.js'
-import connectDb from './config/db.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
 
-connectDb()
+connectDB()
 
 const importData = async () => {
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
-        const createdUser = await User.insertMany(users)
-        const adminUser = createdUser[0]._id
-        const sapmleProduct = products.map(product => {
-            return {
-                ...product,user:adminUser
-            }
-        })
-        await Product.insertMany(sapmleProduct)
-        console.log('Data Imported'.green.inverse)
-        process.exit(1)
-    }
-    catch(error) {
-        console.log(`${error}`.red.inverse)
-        process.exit(1)
-    }
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    const createdUsers = await User.insertMany(users)
+
+    const adminUser = createdUsers[0]._id
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data Imported!'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
+
 const destroyData = async () => {
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
-        
-        console.log('Data Destroyed!'.red.inverse)
-        process.exit(1)
-    }
-    catch(error) {
-        console.log(`${error}`.red.inverse)
-        process.exit(1)
-    }
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log('Data Destroyed!'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
-if(process.argv[2] === '-d'){
-    destroyData()
+
+if (process.argv[2] === '-d') {
+  destroyData()
 } else {
-    importData()
+  importData()
 }
