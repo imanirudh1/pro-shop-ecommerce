8577580a7c9cd580b0588c6437f08f8eb601d38b
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-06 23:59:55 +0530

    authmiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token
+    
+//   if (
+//     req.headers.authorization &&
+//     req.headers.authorization.startsWith('Bearer')
+//   ) {
+//     try {
+//         token = req.header.authorization.split(' ')[1]
+//         console.log(token)
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+//       console.log(decoded)
+//       next()
+//     } catch (error) {}
+//   }
+//   if (!token) {
+//     res.status(401)
+//     throw new Error('Not Authorization, no token')
+//   }
+})
+
+export { protect }
