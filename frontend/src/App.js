cecde8008b478302487e099f6e1d979e20ce2fc7
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-09 17:40:59 +0530

    Created Sign In form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import {Container} from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
       <Header />
       <main>
         <Container className='py-3'>
-          <Route path='/' exact component={HomeScreen} />
-          <Route path='/product/:id' exact component={ProductScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
       <Footer />
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,17 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { } from 'react-bootstrap'
-import { useSelector, useDispatch } from 'react-redux'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../component/Message'
+import Loader from '../component/Loader'
+import FormContainer from '../component/FormContainer'
 import { login } from '../action/userAction'
 
-const LoginScreen = () => {
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    return (
-        <div>
-            
-        </div>
-    )
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  return (
+    <FormContainer>
+          <h1>Sign in</h1>
+          
+          <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+              
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+              </Form.Group>
+              
+        <Button type='submit' variant='primary'>
+          Sign In
+        </Button>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
 }
 
 export default LoginScreen
