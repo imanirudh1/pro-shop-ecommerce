bf67ce674a9941a7bfb38c7205815cf8fdfb7007
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-11 23:25:19 +0530

    Added function to update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,6 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
-
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
@@ -27,27 +26,49 @@ const authUser = asyncHandler(async (req, res) => {
 
 // @desc    GET user profile
 // @route   GET /api/users/profile
-// @access  Public
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  
   // const user = await User.findById(req.user._id )
   const user = await User.findById(req.user._id)
-  
+
   if (user) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin
+      isAdmin: user.isAdmin,
     })
   } else {
     res.status(404)
     throw new Error('User not found')
   }
-
-  
 })
 
+// @desc    UPDATE user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+    const updatedUser = await user.save()
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
 // @desc    Register a new user
 // @route   POST /api/users
@@ -82,4 +103,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser,getUserProfile,registerUser }
+export { authUser, getUserProfile, registerUser, updateUserProfile }
