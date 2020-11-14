e3771d5570ceaa714f14cef19e1012edadb42e94
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-14 17:57:29 +0530

    Created Payment Screen

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { Form, Button, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { savePaymentMethod } from '../action/cartActions'
+import CheckoutSteps from '../component/CheckoutSteps'
+import FormContainer from '../component/FormContainer'
+
+const PaymentScreen = ({ history }) => {
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddress } = cart
+  if (!shippingAddress) {
+    history.push('/shipping')
+  }
+  const [paymentMethod, setPaymentMethod] = useState('PayPal')
+
+  const dispatch = useDispatch()
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(savePaymentMethod(paymentMethod))
+    history.push('/placeorder')
+  }
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h2>Payment Method</h2>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as='legend'>Select Method</Form.Label>
+
+          <Col>
+            <Form.Check
+              type='radio'
+              label='PayPal or Credit Card'
+              id='PayPal'
+              name='paymentMethod'
+              value='PayPal'
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            {/* <Form.Check
+              type='radio'
+              label='Stripe'
+              name='paymentMethod'
+              id='Stripe'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check> */}
+          </Col>
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default PaymentScreen
