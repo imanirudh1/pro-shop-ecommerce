b42ccccb52d2b68fb934cbcf3e9283106117a176
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-22 20:12:24 +0530

    Added getUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,4 +103,12 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile }
+// @desc    GET all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
