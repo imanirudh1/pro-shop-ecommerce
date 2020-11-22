dc270e93ad7d7012414391266e490ad55a04fadc
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-22 20:13:48 +0530

    Added function to check the user is Admin or not

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import e from 'express'
 
 const protect = asyncHandler(async (req, res, next) => {
   let token
@@ -14,12 +15,12 @@ const protect = asyncHandler(async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-      req.user=await User.findById(decoded.id).select('-password')
+      req.user = await User.findById(decoded.id).select('-password')
       next()
     } catch (error) {
-        console.error(error)
-        res.status(401)
-        throw new Error('Not Authorized token failed')
+      console.error(error)
+      res.status(401)
+      throw new Error('Not Authorized token failed')
     }
   }
 
@@ -29,4 +30,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-export { protect }
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as a admin')
+  }
+}
+
+export { protect, admin }
