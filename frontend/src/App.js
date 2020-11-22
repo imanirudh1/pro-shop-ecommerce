a772ddadd1467209ef3ddf5ab2f08b3b93f694dd
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-22 21:02:02 +0530

    Added userListScreen route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
