add4d902733742e07ca589cd4e85b3aade28d2cd
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-23 23:22:04 +0530

    Added delete, update and get user  route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,12 @@ import express from 'express'
 const router = express.Router()
 import {
   authUser,
+  deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
@@ -15,5 +18,10 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
