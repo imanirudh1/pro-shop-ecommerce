f0e30b0a88632acf241f9cd5f32456c57693856d
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-23 23:19:27 +0530

    Fixed some security issues

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,18 +4,30 @@ import { Table, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../component/Message'
 import Loader from '../component/Loader'
-import { listUsers } from '../action/userAction'
+import { deleteUser, listUsers } from '../action/userAction'
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch()
   const userList = useSelector((state) => state.userList)
   const { users, loading, error } = userList
 
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete
+
   useEffect(() => {
-    dispatch(listUsers())
-  }, [dispatch])
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history, successDelete, userInfo])
   const deleteHandler = (id) => {
-    console.log('delete')
+    if (window.confirm('Are You Sure')) {
+      dispatch(deleteUser(id))
+    }
   }
   return (
     <>
@@ -51,7 +63,7 @@ const UserListScreen = () => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
                     <Button className='btn-sm' variant='light'>
                       <i className='fas fa-edit'></i>
                     </Button>
