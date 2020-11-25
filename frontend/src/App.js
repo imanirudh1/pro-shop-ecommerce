d5a19a15c07b15ec4e000abd7ab13d9c2bb7985c
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-11-25 19:26:17 +0530

    Added ProductListScreen route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
           <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
