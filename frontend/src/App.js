166758c4382750dc8fea74539dcfd6b1fc729846
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-01 22:13:57 +0530

    Added orderListScreen route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
 import ProductEditScreen from './screens/ProductEditScreen'
+import OrderListScreen from './screens/OrderListScreen'
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/profile' component={ProfileScreen} />
