9da1959002bcef70e4e284a4d016fea077461039
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-02 22:58:28 +0530

    Added user in the review schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,20 +1,28 @@
 import mongoose from 'mongoose'
 
-const reviewSchema = mongoose.Schema({
-  name:{type:String,required:true},
-  rating:{type:Number,required:true},
-  comment:{type:String,required:true}
-}, {
-  timestamps:true
-})
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
 const productSchema = mongoose.Schema(
-    {
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-        ref:'User'
-      },
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
     name: {
       type: String,
       required: true,
@@ -22,7 +30,6 @@ const productSchema = mongoose.Schema(
     image: {
       type: String,
       required: true,
-      
     },
     brand: {
       type: String,
@@ -31,35 +38,32 @@ const productSchema = mongoose.Schema(
     category: {
       type: String,
       required: true,
-     
     },
     description: {
       type: String,
       required: true,
-     
     },
-    reviews:[reviewSchema]
-    ,
+    reviews: [reviewSchema],
     rating: {
       type: Number,
       required: true,
-     default:0
+      default: 0,
     },
     numReviews: {
       type: Number,
       required: true,
-     default:0
+      default: 0,
     },
     price: {
       type: Number,
       required: true,
-     default:0
+      default: 0,
     },
     countInStock: {
       type: Number,
       required: true,
-     default:0
-    }
+      default: 0,
+    },
   },
   { timestamps: true }
 )
