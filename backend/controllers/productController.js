451ed3cb2cfd4bc5bd373b9654555403ab4c47ee
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-12-04 21:18:06 +0530

    Added getTopRated fuction

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -136,7 +136,16 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Get Top Rated products
+// @route   POST /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+  res.json(products)
+})
+
 export {
+  getTopProducts,
   getProductById,
   getProducts,
   deleteProduct,
